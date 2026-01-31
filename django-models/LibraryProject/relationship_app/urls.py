d79a7b84400c3e51533aab68ade014f11ab1f8ea
karimbyd10 <zrika9324@gmail.com>
2026-01-31 11:34:29 +0100

    urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,3 +21,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 ]
 
+urlpatterns = [
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
+]
+
