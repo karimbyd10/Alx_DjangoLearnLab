74556987f96046e58fccb43b33c9f27a909a7b54
karimbyd10 <zrika9324@gmail.com>
2026-01-31 11:56:12 +0100

    admin.py

---
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Fields", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Fields", {
+            "fields": ("date_of_birth", "profile_photo"),
+        }),
+    )
+
