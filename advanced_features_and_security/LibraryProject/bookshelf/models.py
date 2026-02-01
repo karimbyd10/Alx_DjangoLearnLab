43870f47b1157f815a9c8c4e738ea720f1ed83b5
karimbyd10 <zrika9324@gmail.com>
2026-02-01 09:28:03 +0100

    models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -39,4 +39,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view articles"),
+            ("can_create", "Can create articles"),
+            ("can_edit", "Can edit articles"),
+            ("can_delete", "Can delete articles"),
+        ]
+
+    def __str__(self):
+        return self.title
 
