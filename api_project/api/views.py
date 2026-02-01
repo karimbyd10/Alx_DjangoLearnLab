e22af55546e91ce75cc6995da136dbcbdcc7b68d
karimbyd10 <zrika9324@gmail.com>
2026-02-01 12:10:50 +0100

    views.py

---
+++ api_project/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    """
+    API endpoint that allows all books to be viewed.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
