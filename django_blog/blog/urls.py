97bd797f90e7de2104a99f8838237397befe86f9
karimbyd10 <zrika9324@gmail.com>
2026-02-20 22:19:01 +0000

    urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
@@ -16,4 +23,12 @@ urlpatterns = [
     ), name='logout'),
 
     path('profile/', views.profile, name='profile'),
+]
+urlpatterns = [
+    # Blog CRUD URLs
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
