5efa14efacea6072fec05805b2deb128075e86a7
karimbyd10 <zrika9324@gmail.com>
2026-02-20 23:06:00 +0000

    urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,17 +14,12 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 
-    path('login/', auth_views.LoginView.as_view(
-        template_name='blog/login.html'
-    ), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
 
-    path('logout/', auth_views.LogoutView.as_view(
-        template_name='blog/logout.html'
-    ), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view( template_name='blog/logout.html'), name='logout'),
 
     path('profile/', views.profile, name='profile'),
-]
-urlpatterns = [
+
     path('posts/', PostListView.as_view(), name='post_list'),
 
     path('post/new/', PostCreateView.as_view(), name='post_create'),
@@ -34,4 +29,10 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
 
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+
+    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(),name='comment_create'),
+
+    path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update' ),
+
+    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete' ),
 ]
