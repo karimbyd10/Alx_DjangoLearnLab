d9113272315b9831abf0438cfd912e733e3f5291
karimbyd10 <zrika9324@gmail.com>
2026-02-20 22:39:45 +0000

    models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -11,8 +12,22 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
         return self.title
     
     def get_absolute_url(self):
