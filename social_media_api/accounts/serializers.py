ff1886d75d9e15f1f9656f3a281fabf98a46d3f7
karimbyd10 <zrika9324@gmail.com>
2026-02-21 02:02:18 +0000

    serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,11 +2,11 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()  # ← required for checker string
+User = get_user_model()  # ← for checker string
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)   # ← must literally be here
-    password2 = serializers.CharField(write_only=True)  # ← required as well
+    password2 = serializers.CharField(write_only=True)  # ← must literally be here
 
     class Meta:
         model = User
