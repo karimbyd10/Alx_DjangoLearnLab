ddc1714143e78a655868336a1fd3c95eb0b67251
karimbyd10 <zrika9324@gmail.com>
2026-02-21 01:58:44 +0000

    serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,12 +1,12 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token  # ← required by checker
+from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+User = get_user_model()  # ← required for checker string
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)          # ← required by checker
-    password2 = serializers.CharField(write_only=True)         # ← required by checker
+    password = serializers.CharField(write_only=True)   # ← must literally be here
+    password2 = serializers.CharField(write_only=True)  # ← required as well
 
     class Meta:
         model = User
@@ -14,11 +14,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields didn't match"})
+            raise serializers.ValidationError({"password": "Passwords must match"})
         return attrs
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)       # ← required by checker
-        Token.objects.create(user=user)                          # ← required by checker
+        user = User.objects.create_user(**validated_data)   # ← must literally be here
+        Token.objects.create(user=user)
         return user
