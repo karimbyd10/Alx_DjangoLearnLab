d8da102d3056327dd15cf6da538b73ecdc13e7a8
karimbyd10 <zrika9324@gmail.com>
2026-02-21 01:44:08 +0000

    serializers.py and models.py

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'password2', 'bio', 'profile_picture')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn’t match."})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'following')
