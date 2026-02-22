af8de509093fd2f0ba52024ad2619e84de83de7b
karimbyd10 <zrika9324@gmail.com>
2026-02-22 22:59:08 +0000

    Add explicit database configuration with PORT

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -102,6 +102,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_media_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
