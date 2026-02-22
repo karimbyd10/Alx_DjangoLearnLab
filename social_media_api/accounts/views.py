df83280e08ffe36a678d124a6894ea0212904ef1
karimbyd10 <zrika9324@gmail.com>
2026-02-22 17:31:16 +0000

    views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework.views import APIView
 from django.contrib.auth import authenticate
 from .serializers import UserRegisterSerializer, UserProfileSerializer
 from .models import CustomUser
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 
 # Registration
 class RegisterView(generics.CreateAPIView):
@@ -36,3 +38,30 @@ class UserProfileView(generics.RetrieveAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=["post"])
+    def follow(self, request, pk=None):
+        user_to_follow = self.get_object()
+
+        if request.user == user_to_follow:
+            return Response(
+                {"detail": "You cannot follow yourself."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        request.user.following.add(user_to_follow)
+
+        return Response({"detail": "Successfully followed user."})
+
+    @action(detail=True, methods=["post"])
+    def unfollow(self, request, pk=None):
+        user_to_unfollow = self.get_object()
+
+        request.user.following.remove(user_to_unfollow)
+
+        return Response({"detail": "Successfully unfollowed user."})
